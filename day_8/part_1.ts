eb4c520e8dea9bef920a0f623930aabe0bdaa135
Lukas Reining <lukas.reining@codecentric.de>
2022-12-11 16:22:51 +0100

    Day 8, Part 1

---
+++ day_8/part_1.ts
@@ -0,0 +1,61 @@
+import {
+  DecimalMatrix,
+  Matrix,
+  readFileAsString,
+  toDecimalMatrix,
+} from "../helpers/parsing";
+import { toSum } from "../helpers/math";
+
+export function isLargest(row: number[], value: number) {
+  return row.every((currentValue) => currentValue < value);
+}
+
+export function column(matrix: DecimalMatrix, number: number) {
+  return matrix.map(function (value, index) {
+    return value[number];
+  });
+}
+
+export function countVisible(matrix: Matrix<boolean>) {
+  return matrix
+    .flatMap((row) => row.map((visible) => (visible ? 1 : 0)))
+    .reduce(toSum, 0);
+}
+
+export function solve() {
+  const text = readFileAsString("./input.txt");
+  const map = toDecimalMatrix(text);
+
+  return map.reduce((visibilityMatrix, colum, columnIndex, mapMatrix) => {
+    return [
+      ...visibilityMatrix,
+      colum.map<boolean>((height, rowIndex, mapRow) => {
+        const visibleFromLeft = isLargest(mapRow.slice(0, rowIndex), height);
+        const visibleFromRight = isLargest(mapRow.slice(rowIndex + 1), height);
+
+        const visibleFromTop = isLargest(
+          column(mapMatrix, rowIndex).slice(0, columnIndex),
+          height
+        );
+
+        const visibleFromBottom = isLargest(
+          column(mapMatrix, rowIndex).slice(columnIndex + 1),
+          height
+        );
+
+        return (
+          visibleFromBottom ||
+          visibleFromTop ||
+          visibleFromLeft ||
+          visibleFromRight
+        );
+      }),
+    ];
+  }, new Array<boolean[]>());
+}
+
+const visibilityMap = solve();
+
+console.log(visibilityMap);
+console.log(countVisible(visibilityMap));
+
--- helpers/parsing.ts
+++ helpers/parsing.ts
@@ -1,13 +1,25 @@
-import * as fs from 'fs'
+import * as fs from "fs";
 
 export function readFileAsString(path: string) {
-    return fs.readFileSync(path, 'utf-8')
+  return fs.readFileSync(path, "utf-8");
 }
 
 export function toLines(text: string) {
-    return text.split("\n")
+  return text.split("\n");
+}
+
+export type Matrix<T> = T[][]
+
+export type CharacterMatrix = Matrix<String>
+export function toCharacterMatrix(input: string): CharacterMatrix {
+  return input.split("\n").map((line) => line.split(""));
+}
+
+export type DecimalMatrix = Matrix<number>
+export function toDecimalMatrix(input: string): DecimalMatrix {
+  return input.split("\n").map((line) => line.split("").map(toDecimalInt));
 }
 
 export function toDecimalInt(number: string) {
-    return Number.parseInt(number, 10)
+  return Number.parseInt(number, 10);
 }
